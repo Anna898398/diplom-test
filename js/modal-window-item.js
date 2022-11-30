77d490df24c215b3df1f529db0327b7a3cb6493a
@Anna898398 <nutikparawutik@mail.ru>
2022-11-30 15:46:38 +0300

    исправлены некоторые ошибки в верстке

--- js/modal-window-item.js
+++ js/modal-window-item.js
@@ -1,28 +1,27 @@
-//1__________________________________________________
 $(document).ready(function($) {
     
     $('.box-text-1').click(function() {
-        $('.modal-window-item-1').fadeIn();
+        $('.modal-window-item-1').fadeIn();  //1
         return false;
     });
 
 	$('.box-text-2').click(function() {
-        $('.modal-window-item-2').fadeIn();
+        $('.modal-window-item-2').fadeIn();  //2
         return false;
     });
 
 	$('.box-text-3').click(function() {
-        $('.modal-window-item-2').fadeIn();
+        $('.modal-window-item-3').fadeIn();  //3
         return false;
     });
 
 	$('.box-text-4').click(function() {
-        $('.modal-window-item-2').fadeIn();
+        $('.modal-window-item-4').fadeIn();  //4
         return false;
     });
 
 	$('.box-text-5').click(function() {
-        $('.modal-window-item-2').fadeIn();
+        $('.modal-window-item-5').fadeIn();  //5
         return false;
     });
 
@@ -64,6 +63,70 @@ $(document).ready(function($) {
     
     $('.modal-window-item-2').click(function(e) {
         if ($(e.target).closest('.modal-window-content-2').length === 0) {
+            $(this).fadeOut();					
+        }
+    });
+
+
+
+	//3__________________________________________________
+	$('.btn-close-modal-window-item').click(function() {
+        $(this).parents('.modal-window-item-3').fadeOut();
+        return false;
+    });		
+    
+    $(document).keydown(function(e) {
+        if (e.keyCode === 27) {
+            e.stopPropagation();
+            $('.modal-window-item-3').fadeOut();
+        }
+    });
+    
+    $('.modal-window-item-3').click(function(e) {
+        if ($(e.target).closest('.modal-window-content-3').length === 0) {
+            $(this).fadeOut();					
+        }
+    });
+
+
+
+	//4__________________________________________________
+	$('.btn-close-modal-window-item').click(function() {
+        $(this).parents('.modal-window-item-4').fadeOut();
+        return false;
+    });		
+    
+    $(document).keydown(function(e) {
+        if (e.keyCode === 27) {
+            e.stopPropagation();
+            $('.modal-window-item-4').fadeOut();
+        }
+    });
+    
+    $('.modal-window-item-4').click(function(e) {
+        if ($(e.target).closest('.modal-window-content-4').length === 0) {
+            $(this).fadeOut();					
+        }
+    });
+
+
+
+
+	//5__________________________________________________
+	$('.btn-close-modal-window-item').click(function() {
+        $(this).parents('.modal-window-item-5').fadeOut();
+        return false;
+    });		
+    
+    $(document).keydown(function(e) {
+        if (e.keyCode === 27) {
+            e.stopPropagation();
+            $('.modal-window-item-5').fadeOut();
+        }
+    });
+    
+    $('.modal-window-item-5').click(function(e) {
+        if ($(e.target).closest('.modal-window-content-5').length === 0) {
             $(this).fadeOut();					
         }
     });
